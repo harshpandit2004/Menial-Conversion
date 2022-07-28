a2aea8a90a6c9703e44c922fde3a13773a92ab54
Harsh Pandit <harsh.pandit2004@gmail.com>
2022-07-28 20:06:08 +0530

    Decimal iterations completed
    The decimal to *x* conversion's logics are committed

--- conversions/Decimal/decimal-to-hexadecimal/index.js
+++ conversions/Decimal/decimal-to-hexadecimal/index.js
@@ -1,27 +1,9 @@
-function decToBinaryConvert() {
-  var binary = "";
-  var bitvalue = document.getElementById("bit").value;
-  var decimal_number = document.getElementById("decimal").value;
-
-  for (i = 0; i < bitvalue; i++) {
-    var remainder = decimal_number % 2;
-
-    if (remainder == 0) {
-      binary = binary + "0";
-    } else {
-      binary = binary + "1";
-    }
-
-    decimal_number = Math.trunc(decimal_number / 2);
-  }
-
-  var binaryStr1 = binary.split("");
-  var binaryStr2 = binaryStr1.reverse();
-  var binaryStrFinal = binaryStr2.join("");
-  document.getElementById("bin").value = binaryStrFinal;
+function decTohexConvert() {
+  var dec = document.getElementById("decimal").value;
+  var hex =  Math.abs(dec).toString(16);
+  document.getElementById("hex").value = hex;
 }
-function decToBinaryReset() {
+function decTohexReset() {
   document.getElementById("decimal").value = "";
-  document.getElementById("bit").value = "";
-  document.getElementById("bin").value = "";
+  document.getElementById("hex").value = "";
 }
