381560f0ee8dd1a8ab7ce6e7123f983863aec9ba
Harsh Pandit <harsh.pandit2004@gmail.com>
2022-07-28 19:22:15 +0530

    initial commit

---
+++ conversions/Decimal/decimal-to-octal/index.js
@@ -0,0 +1,27 @@
+function decToBinaryConvert() {
+  var binary = "";
+  var bitvalue = document.getElementById("bit").value;
+  var decimal_number = document.getElementById("decimal").value;
+
+  for (i = 0; i < bitvalue; i++) {
+    var remainder = decimal_number % 2;
+
+    if (remainder == 0) {
+      binary = binary + "0";
+    } else {
+      binary = binary + "1";
+    }
+
+    decimal_number = Math.trunc(decimal_number / 2);
+  }
+
+  var binaryStr1 = binary.split("");
+  var binaryStr2 = binaryStr1.reverse();
+  var binaryStrFinal = binaryStr2.join("");
+  document.getElementById("bin").value = binaryStrFinal;
+}
+function decToBinaryReset() {
+  document.getElementById("decimal").value = "";
+  document.getElementById("bit").value = "";
+  document.getElementById("bin").value = "";
+}
